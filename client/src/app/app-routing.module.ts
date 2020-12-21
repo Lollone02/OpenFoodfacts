a4b4f68da9724bb361b21b2b424af7d94dd380c0
LorenzoBiraghi <biraghi.lorenzo@iisgalvanimi.edu.it>
2020-12-21 08:36:54 +0000

    Punto 2

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { ProdottoComponent } from './prodotto/prodotto.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'search', component: SearchComponent}, {path:'prodotto', component: ProdottoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
